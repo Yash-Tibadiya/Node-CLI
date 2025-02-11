a59c75c702a6b82b7213359fb54d2c1b03112a10
Yash-Tibadiya <tibadiyayash@gmail.com>
2025-02-11 12:32:05 +0530

    initialize command-line utility with basic structure and dependencies

---
+++ index.ts
@@ -0,0 +1,20 @@
+import chalk from "chalk";
+import figlet from "figlet";
+
+//! Header section
+console.log();
+console.log(chalk.magenta("Welcome to"));
+console.log(
+  chalk.magentaBright(
+    figlet.textSync("Yash's Node CLI", {
+      horizontalLayout: "full",
+      font: "Doom",
+    })
+  )
+);
+console.log(
+  chalk.cyan(
+    "Yash's Node CLI is a handy command-line tool for everyday tasks like file compression, string manipulation, and API integration."
+  )
+);
+console.log();
