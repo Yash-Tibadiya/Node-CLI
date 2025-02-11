be6672a369cd9b8cb3713a21bfd1978310c4474a
Yash-Tibadiya <tibadiyayash@gmail.com>
2025-02-11 12:58:43 +0530

    add inquirer dependency and implement user interaction for CLI commands

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import { program } from "commander";
 import chalk from "chalk";
 import figlet from "figlet";
+import inquirer from "inquirer";
 
 //! Header section
 console.log();
@@ -20,9 +21,46 @@ console.log(
 );
 console.log();
 
+//! CLI commands
 program
   .name("Yash's Node CLI")
   .description(
     "Yash's Node CLI is a handy command-line tool for everyday tasks like file compression, string manipulation, and API integration."
   )
   .version("1.0.0");
+
+program.action(() => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Hello! What's your name? :",
+      },
+    ])
+    .then((answer) => {
+      const userName = answer.name;
+      console.log(chalk.italic.magentaBright(`Hello, ${userName}! 🙋🏻`));
+
+      function menu() {
+        console.log();
+        inquirer
+          .prompt([
+            {
+              type: "list",
+              name: "task",
+              message: "What would you like to do?",
+              choices: [
+                "Compress a file",
+                "Decompress a file",
+                "Convert case of text",
+                "Count words in a text",
+                "Check if text is a palindrome",
+                "Get a Random Joke",
+                "Exit",
+              ],
+            },
+          ])
+      }
+    });
+});
